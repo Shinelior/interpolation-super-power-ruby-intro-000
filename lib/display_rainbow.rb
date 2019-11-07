bbb16cd717a6ec5ea6a4ab46fa6186d9ad301423
lior Shine <shinelior@gmail.com>
2019-11-07 23:38:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,7 +8,11 @@ colors1=["red,","orange,","yellow,","green,","blue,","indigo,","violet,"]
  colors2 =letters.zip(colors1)
   
 
-colors=colors2[1]
+colors=colors2[0]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors[1]
 display_rainbow(colors)
 colors=["red","orange","yellow","green","blue","indigo","violet"]
 
@@ -17,20 +21,16 @@ display_rainbow(colors)
 colors=["red","orange","yellow","green","blue","indigo","violet"]
 
 colors=colors[3]
+
+
 display_rainbow(colors)
 colors=["red","orange","yellow","green","blue","indigo","violet"]
 
 colors=colors[4]
-
-
 display_rainbow(colors)
 colors=["red","orange","yellow","green","blue","indigo","violet"]
 
 colors=colors[5]
-display_rainbow(colors)
-colors=["red","orange","yellow","green","blue","indigo","violet"]
-
-colors=colors[6]
 display_rainbow(colors)
 
 
