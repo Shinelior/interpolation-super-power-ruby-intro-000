026ff6581480aff2a06911dd03a6ccb0333a1e06
lior Shine <shinelior@gmail.com>
2019-11-07 23:05:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,34 @@
 def display_rainbow(colors)
   
  colors=["red","orange","yellow","green","blue","indigo","violet"]
- letters=["R:","O:","Y:","G:","B:","I:","V:"]
-  
+ colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+
+colors=colors[1]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors[2]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors[3]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors[4]
+
+
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors[5]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors[6]
+display_rainbow(colors)
+
   print letters.zip(colors)
   
 end
